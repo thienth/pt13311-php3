18aa90bb76564585ce7be34016821875748b0b4d
thienth32 <thienth32@gmail.com>
2019-08-01 14:46:34 +0700

    login form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
-Route::get('/', 'PostController@homepage')->name('homepage');
-Route::get('post-remove/{id}', 'PostController@remove')->name('post.remove');
-Route::get('post/add', 'PostController@addForm')->name('post.add');
-Route::post('post/add', 'PostController@saveAdd');
+
+
+Route::get('/', 'PostController@homepage')->middleware('auth')->name('homepage');
+Route::get('post-remove/{id}', 'PostController@remove')->middleware('auth')->name('post.remove');
+Route::get('post/add', 'PostController@addForm')->middleware('auth')->name('post.add');
+Route::post('post/add', 'PostController@saveAdd')->middleware('auth');
+
+Route::get('cp-login', 'Auth\LoginController@loginForm')->name('login');
